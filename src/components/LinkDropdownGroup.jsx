5c307c52121df253ef72ec377bff46e622e70f4a
faramozzayw <faramo.zayw@gmail.com>
2020-03-07 18:04:12 +0200

    Update render links

--- src/components/LinkDropdownGroup.jsx
+++ src/components/LinkDropdownGroup.jsx
@@ -1,17 +1,22 @@
-import React from "react";
-import { NavbarItem, NavbarDropdown } from "bloomer";
+import React, { useState } from "react";
+import { NavbarDropdown, NavbarItem, Icon } from "bloomer";
 
 import NavbarLink from "./NavbarLink";
 
-const LinkDropdownGroup = ({ path, description, icon_name, children }) => (
-    <NavbarItem hasDropdown isHoverable>
-	    <NavbarLink path={path} description={description} icon_name={icon_name} />
-	    <NavbarDropdown>
-			{
-				children
-			}
-	    </NavbarDropdown>
-	</NavbarItem>
-);
+const LinkDropdownGroup = ({ path, description, icon_name, children }) => {
+    const [showStatus, updateShowStatus] = useState(false)
+
+    return (
+		<NavbarItem hasDropdown isHoverable={false} isActive={showStatus}>
+			<div className="arrow">
+        	    <NavbarLink path={path} description={description} icon_name={icon_name} />
+        	    <Icon className="fa fa-arrow-down" isSize="medium" onClick={() => updateShowStatus(!showStatus)}/>
+        	</div>
+			<NavbarDropdown isHidden={!showStatus}>
+				{ children }
+			</NavbarDropdown>
+		</NavbarItem>
+    )
+}
 
 export default LinkDropdownGroup;
