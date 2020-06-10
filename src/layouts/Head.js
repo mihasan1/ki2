4408d591d68b5db951318ff01bce69d1e57b229c
faramo_zayw <faramo.zayw@gmail.com>
2020-06-10 11:17:04 +0000

    Updated Head component

--- src/layouts/Head.js
+++ src/layouts/Head.js
@@ -16,9 +16,39 @@ const Head = ({ path }) => {
 		<Helmet>
 			<title>Київський авіаційний технікум {title} </title>
 			<meta charset="utf-8" />
-			<meta name="description" content="Київський авіаційний технікум" />
-			<meta name="keywords" content="Київський авіаційний технікум, Київський авіаційний технікум сайт, КАТ, КіАТ" />
-			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			<meta
+				http-equiv="Content-Type"
+				content="text/html; charset=UTF-8"
+			/>
+			<meta 
+				name="description"
+				content={`Київський авіаційний технікум ${title}`}
+			/>
+			<meta
+				name="keywords"
+				content="Київський авіаційний технікум, Київський, авіаційний технікум, сайт, КАТ, КіАТ, киат" 
+			/>
+			<meta
+				name="viewport" 
+				content="width=device-width, initial-scale=1.0"
+			/>
+			<meta
+				name="robots"
+				content="all"
+			/>
+			<meta
+				name="Author"
+				content="Київський авіаційний технікум" 
+			/>
+			<meta 
+				name="Copyright"
+				content="Київський авіаційний технікум"
+			/>
+			<meta
+				name="google"
+				content="notranslate"
+			/>
+			
 		</Helmet>
 	)
 }
