71924ee95fa69a86de707ab03a15c46cc1dc3ce6
faramo_zayw <40602472+faramozzayw@users.noreply.github.com>
2020-06-18 18:32:01 +0300

    Update styling (#38)
    * Update text and link style
    
    * Update news style

--- src/components/RawMD/RawMarkdown.jsx
+++ src/components/RawMD/RawMarkdown.jsx
@@ -3,17 +3,17 @@ import { Container, Title, Content, Box } from "bloomer";
 
 const RawMarkdown = ({ html }) => {
 	return (
-			<Container>
-				<Box>
-					<Content>
-                        <div 
-                            dangerouslySetInnerHTML={{ __html: html }}
-                            className="raw-markdown-remark"
-                        />
-					</Content>
-				</Box>
-			</Container>
+		<Container>
+			<Box>
+				<Content>
+					<div
+						dangerouslySetInnerHTML={{ __html: html }}
+						className="raw-markdown-remark"
+					/>
+				</Content>
+			</Box>
+		</Container>
 	);
-}
+};
 
 export default RawMarkdown;
--- src/template/newsTemplate.jsx
+++ src/template/newsTemplate.jsx
@@ -2,15 +2,11 @@ import React from "react";
 
 import { graphql } from "gatsby";
 
-import { 
-	Image,
-	Content,
-	Box,
-	Title,
-	Container,
-} from "bloomer";
+import { Image, Content, Box, Title, Container } from "bloomer";
 
 import Layout from "./../layouts/Layout";
+
+import { RawMarkdown } from "./../components";
 
 const NewsTemplate = ({ data, location }) => {
 	const { markdownRemark } = data;
@@ -21,32 +17,19 @@ const NewsTemplate = ({ data, location }) => {
 		<Layout location={location}>
 			<Container>
 				<Box>
-					<Box>
-						<Title isSize={4} tag="h4">
-							# { !!title ? title : "Новина без назви - просто новина" }
-						</Title>
-					</Box>
-					<Box>
-						<Content>
-							<div
-								dangerouslySetInnerHTML={{ __html: html }}
-							/>
-							<br/>
-						</Content>
-						
-						<small>Дата публікації: <time>{date}</time></small>
-					</Box>
+					<Title isSize={4} tag="h4">
+						# {title ?? "Новина без назви - просто новина"}
+					</Title>
 				</Box>
+				<RawMarkdown html={html} />
 			</Container>
 		</Layout>
 	);
-}
+};
 
 export const pageQuery = graphql`
 	query($path: String!) {
-		markdownRemark(frontmatter: {
-			path: { eq: $path } 
-		}) {
+		markdownRemark(frontmatter: { path: { eq: $path } }) {
 			html
 			frontmatter {
 				date(formatString: "D MMMM, YYYY", locale: "uk_UA")
@@ -55,6 +38,6 @@ export const pageQuery = graphql`
 			}
 		}
 	}
-`
+`;
 
 export default NewsTemplate;
