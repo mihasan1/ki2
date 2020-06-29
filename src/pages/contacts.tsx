cb9a7c51dc42cc3da8793591b0cf0befa1135aae
faramo_zayw <faramo.zayw@gmail.com>
2020-06-29 15:00:53 +0000

    Update

--- src/pages/contacts.tsx
+++ src/pages/contacts.tsx
@@ -7,28 +7,24 @@ import Layout from "./../layouts/Layout";
 
 import { PageWithLocation } from "./../types";
 
-const space = " ";
-
 const Contacts: React.FC<PageWithLocation> = ({ location }) => (
 	<Layout location={location}>
 		<Container hasTextAlign="centered">
-			<Content>
+			<Content className="raw-markdown-remark">
 				<Title>Київський авіаційний технікум</Title>
 				<hr />
 				<Heading>м. Київ 03062 пр. Перемоги, 100/1</Heading>
 
 				<p>
-					тел.:{space}
-					<a href="tel:+380444430203">(044) 443-02-03</a>,{space}
+					тел.: <a href="tel:+380444430203">(044) 443-02-03</a>,{" "}
 					<a href="tel:+380444002881">(044) 400-28-81</a> - приймальна директора
 					<br />
 					<a href="tel:+3800444545762">(044) 454-57-62</a> - навчальна частина
 				</p>
 
 				<p>
-					моб.:{space}
-					<a href="tel:+380953914188">(095) 391-41-88</a>,{space}
-					<a href="tel:+380686865815">(068) 686-58-15</a>,{space}
+					моб.: <a href="tel:+380953914188">(095) 391-41-88</a>,{" "}
+					<a href="tel:+380686865815">(068) 686-58-15</a>,{" "}
 					<a href="tel:+380937103158">(093) 710-31-58</a> - приймальна комісія
 				</p>
 
